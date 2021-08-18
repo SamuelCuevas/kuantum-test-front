066307e404e1f76c93ddf889189ff84ad0c5e7ee
Samuel Cuevas <samuelignacio46@gmail.com>
2021-08-17 21:52:43 -0400

    Advance
    Finish edit device and alerts simulation

--- src/components/device/Device.js
+++ src/components/device/Device.js
@@ -1,44 +1,29 @@
 import React, { useState } from 'react'
 import { fetchDevice } from '../../helpers/fetch';
 import { useForm } from '../../hooks/useForm'
+import Swal from "sweetalert2";
 
 export const Device = () => {
 
     const [values, handleInputChange] = useForm();
-    const [data, setData] = useState();
-    // const data = {
-    //     "uuid": values.uuid,
-    //     "name": values.name,
-    //     "description": values.description,
-    //     "alerts_config": {
-    //         "name": values.alert_name,
-    //         "alert_type": values.alert_type,
-    //         "value": values.alert_value,
-    //         "range": {
-    //             "min": values.alert_min,
-    //             "max": values.alert_max
-    //         }
-    //     }
-    // }
-    
+    const [data, setData] = useState(values);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault();
-        setD();
-        if(data){
-            fetchDevice("new",data,"POST");
+        if(!values.uuid || !values.name || !values.description || !values.alert_name || !values.alert_type || !values.value || !values.min || !values.max){
+            Swal.fire( 'Error', 'Try again', 'error' );
         }else{
-            alert('intente nuevamente');
-        }
-    }
 
-    const handleSearch = (e) => {
-        e.preventDefault();
-        setD();
-        if(data){
-            console.log(data);
-        }else{
-            alert('intente nuevamente');
+            // ARREGLAR ESTO CON RESPUESTA DEL BACKEND
+            await setD();
+            if(data === {}){
+                Swal.fire( 'Error', 'Try again', 'error' );
+                
+            } else {
+                await fetchDevice("new",data,"POST").then(res => {
+                    Swal.fire('Success', res.msg, 'success');
+                });
+            }
         }
     }
 
@@ -50,10 +35,10 @@ export const Device = () => {
             "alerts_config": {
                 "name": values.alert_name,
                 "alert_type": values.alert_type,
-                "value": values.alert_value,
+                value: values.value,
                 "range": {
-                    "min": values.alert_min,
-                    "max": values.alert_max
+                    min: values.min,
+                    max: values.max
                 }
             }
         });
@@ -79,20 +64,19 @@ export const Device = () => {
                                     <h2 className="title_form">Alerts Config</h2>
                                         <div className="input-group mb-3">
                                             <label className="input-group-text">Name</label>
-                                            <input type="text" className="form-control" id="alert_name" name="alert_name" onChange= { handleInputChange }/>
+                                            <input type="text" className="form-control" name="alert_name" onChange= { handleInputChange }/>
                                             <label className="input-group-text">Alert Type</label>
                                             <input type="text" className="form-control" id="alert_type" name="alert_type" onChange= { handleInputChange }/>
                                         </div>
                                         <div className="input-group mb-3">
                                             <label className="input-group-text">Value</label>
-                                            <input type="number" className="form-control" step="0.01" id="alert_value" name="alert_value" onChange= { handleInputChange }/>
+                                            <input type="number" className="form-control" step="0.01" id="value" name="value" onChange= { handleInputChange }/>
                                             <span className="input-group-text">Range</span>
-                                            <input type="number" className="form-control" placeholder="Min" step="0.01" id="alert_min" name="alert_min" onChange= { handleInputChange }/>
-                                            <input type="number" className="form-control" placeholder="Max" step="0.01" id="alert_max" name="alert_max" onChange= { handleInputChange }/>
+                                            <input type="number" className="form-control" placeholder="Min" step="0.01" id="min" name="min"  onChange= { handleInputChange }/>
+                                            <input type="number" className="form-control" placeholder="Max" step="0.01" id="max" name="max"  onChange= { handleInputChange }/>
                                         </div>
                                 </div>
-                            <button type="submit" className="btn btn-primary" onClick={ handleSubmit }>Submit</button>
-                            <button className="btn btn-primary " onClick={ handleSearch }>Search</button>
+                            <button type="submit" className="btn btn-primary form-control" onClick={ handleSubmit }>Submit</button>
                         </form>
                 </div>
             </div>
--- src/components/device/DeviceList.js
+++ src/components/device/DeviceList.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchDevice } from '../../helpers/fetch';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import Swal from "sweetalert2";
+
+moment.locale('es');
+
 
 export const DeviceList = () => {
+
+    const [devices, setDevices] = useState([]);
+    const getDevices = async() => {
+        const body = await fetchDevice("");
+        const resp = await body.json();
+        if(resp.ok){
+            setDevices(resp.devices);
+        }
+    }
+
+    useEffect(() => {
+        getDevices();
+        
+    }, []);
+    
+    const handleDelete = async(e) => {
+        e.preventDefault();
+        const resp = await fetchDevice(e.target.value, {}, 'DELETE');
+        const body = await resp.json();
+        if(body.ok) {
+            Swal.fire('Success', body.msg, 'success');
+        }
+    }
+
     return (
         <>
             <div className="container">
@@ -15,13 +46,24 @@ export const DeviceList = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <th><button id="delete_device" name="delete_device" className="btn"><i className='bx bxs-x-circle del'></i></button></th>
-                                </tr>
+                                {
+                                    devices.map((dev, i) => (
+                                        <tr key={ i }>
+                                            <td>{ dev.uuid }</td>
+                                            <td>{ dev.name }</td>
+                                            <td>{ dev.description }</td>
+                                            <td>{ moment(dev.createdAt).format('hh:mm:ss-DD/MM/YY') }</td>
+                                            <td><button className="btn btn-danger bt" name="btnDelete" id={ dev.uuid } value={ dev.uuid } onClick={ handleDelete }>X</button>
+                                                <Link to={ `./device/${dev.uuid}` }>
+                                                <i className='bx bxs-pencil bti'></i>
+                                                </Link>
+                                            </td>
+
+                                        </tr>                                            
+                                    ))
+                                    
+                                }
+                                   
                             </tbody>
                 </table>
             </div>
